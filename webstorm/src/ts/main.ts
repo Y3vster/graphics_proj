a275f21f193cc1f38906209464e8d738c2026312
Yev <YK@macbook-pro.seattleu.edu>
2016-11-09 13:06:39 -0800

    -

---
+++ webstorm/src/ts/main.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by YK on 11/9/16.
+ */
+/**
+ * Created by YK on 11/8/16.
+ */
+
+/// <reference path="./dat.gui.d.ts" />
+/// <reference path="./three.d.ts" />
+
+var FizzyText = function() {
+    this.message = 'dat.gui';
+    this.speed = 0.8;
+    this.displayOutline = false;
+    this.explode = function() {
+
+    };
+};
+
+window.onload = function() {
+    var text = new FizzyText();
+    var gui = new dat.GUI();
+    gui.add(text, 'message');
+    gui.add(text, 'speed', -5, 5);
+    gui.add(text, 'displayOutline');
+    gui.add(text, 'explode');
+};
