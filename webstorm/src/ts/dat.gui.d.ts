65016ddd24f5adb7248a36213895b71e37cda4cb
Yev <YK@zx.local>
2016-11-26 16:20:13 -0800

    revering back to prev version

--- webstorm/src/ts/dat.gui.d.ts
+++ webstorm/src/ts/dat.gui.d.ts
@@ -4,62 +4,120 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace dat {
+
+    export module controllers {
+
+        export class Controller {
+            destroy(): void;
+
+            // Controller
+            onChange: (value?: any) => void;
+            onFinishChange: (value?: any) => void;
+
+            setValue(value: any): Controller;
+
+            getValue(): any;
+
+            updateDisplay(): void;
+
+            isModified(): boolean;
+
+            // NumberController
+            min(n: number): Controller;
+
+            max(n: number): Controller;
+
+            step(n: number): Controller;
+
+            domElement: HTMLElement;
+
+            // FunctionController
+            fire(): Controller;
+
+            // augmentController in dat/gui/GUI.js
+            options(option: any): Controller;
+
+            name(s: string): Controller;
+
+            listen(): Controller;
+
+            remove(): Controller;
+        }
+    }
+    export var GUIController: controllers.Controller;
+    // var GUIController: controllers.Controller;
+
     export class GUI {
         constructor(option?: GUIParams);
 
-        __controllers: GUIController[];
+        __controllers: controllers.Controller[];
         __folders: GUI[];
         domElement: HTMLElement;
 
-        add(target: Object, propName:string): GUIController;
-        add(target: Object, propName:string, min: number, max: number): GUIController;
-        add(target: Object, propName:string, status: boolean): GUIController;
-        add(target: Object, propName:string, items:string[]): GUIController;
-        add(target: Object, propName:string, items:number[]): GUIController;
-        add(target: Object, propName:string, items:Object): GUIController;
+        add(target: Object, propName: string): controllers.Controller;
+        add(target: Object, propName: string, min: number, max: number): controllers.Controller;
+        add(target: Object, propName: string, status: boolean): controllers.Controller;
+        add(target: Object, propName: string, items: string[]): controllers.Controller;
+        add(target: Object, propName: string, items: number[]): controllers.Controller;
+        add(target: Object, propName: string, items: Object): controllers.Controller;
 
-        addColor(target: Object, propName:string): GUIController;
-        addColor(target: Object, propName:string, color: string): GUIController;
-        addColor(target: Object, propName:string, rgba: number[]): GUIController; // rgb or rgba
-        addColor(target: Object, propName:string, hsv:{h:number; s:number; v:number}): GUIController;
+        addColor(target: Object, propName: string): controllers.Controller;
+        addColor(target: Object, propName: string, color: string): controllers.Controller;
+        addColor(target: Object, propName: string, rgba: number[]): controllers.Controller; // rgb or rgba
+        addColor(target: Object, propName: string, hsv: {h: number; s: number; v: number}): controllers.Controller;
 
-        remove(controller: GUIController): void;
+        remove(controller: controllers.Controller): void;
+
         destroy(): void;
 
-        addFolder(propName:string): GUI;
+        addFolder(propName: string): GUI;
 
         open(): void;
+
         close(): void;
 
         remember(target: Object, ...additionalTargets: Object[]): void;
+
         getRoot(): GUI;
 
         getSaveObject(): Object;
-        save(): void;
-        saveAs(presetName:string): void;
-        revert(gui:GUI): void;
 
-        listen(controller: GUIController): void;
+        save(): void;
+
+        saveAs(presetName: string): void;
+
+        revert(gui: GUI): void;
+
+        listen(controller: controllers.Controller): void;
+
         updateDisplay(): void;
 
         // gui properties in dat/gui/GUI.js
         parent(): GUI;
+
         scrollable(): boolean;
+
         autoPlace(): boolean;
+
         preset(): string;
         preset(s: string): void;
+
         width(): number;
         width(n: number): void;
+
         name(): string;
         name(s: string): void;
+
         closed(): boolean;
         closed(b: boolean): void;
+
         load(): Object;
+
         useLocalStorage(): boolean;
         useLocalStorage(b: boolean): void;
     }
 
-    export interface GUIParams{
+    export interface GUIParams {
         autoPlace?: boolean;
         closed?: boolean;
         load?: any;
@@ -68,32 +126,5 @@ declare namespace dat {
         width?: number;
     }
 
-    export class GUIController {
-        destroy(): void;
 
-        // Controller
-        onChange: (value?: any) => void;
-        onFinishChange: (value?: any) => void;
-
-        setValue(value: any): GUIController;
-        getValue(): any;
-        updateDisplay(): void;
-        isModified(): boolean;
-
-        // NumberController
-        min(n: number): GUIController;
-        max(n: number): GUIController;
-        step(n: number): GUIController;
-
-        domElement: HTMLElement;
-
-        // FunctionController
-        fire(): GUIController;
-
-        // augmentController in dat/gui/GUI.js
-        options(option:any):GUIController;
-        name(s: string): GUIController;
-        listen(): GUIController;
-        remove(): GUIController;
-    }
 }
